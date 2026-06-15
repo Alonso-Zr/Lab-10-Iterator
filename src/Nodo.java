33e52ab62a3e051ebd3c2b414e2c3ed9e9b28a22
Alonso-Zr <alonsoseguro27@gmail.com>
2026-06-14 22:25:55 -0500

    feat: crear la clase Nodo

---
+++ src/Nodo.java
@@ -0,0 +1,9 @@
+public class Nodo {
+    Trabajador trabajador;
+    Nodo next;
+
+    public Nodo(Trabajador trabajador) {
+        this.trabajador = trabajador;
+        this.next = null;
+    }
+}
